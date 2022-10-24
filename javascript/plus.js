cc7b4727490b6af09f97051d3a0a04e43a0dac16
wonjoon200 <102003074+wonjoon200@users.noreply.github.com>
2022-10-24 11:32:38 +0900

    10.24,1~3

---
+++ javascript/plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;
+    result=num1+num2; 
+    return result
+}
+function subtraction(num1,num2){
+    let result;
+    result=num1-num2; 
+    return result
+}
+function multiplication(num1,num2){
+    let result;
+    result=num1*num2; 
+    return result
+}
+function division(num1,num2){
+    let result;
+    result=num1/num2; 
+    return result
+}
